2dc0c5a0682a1c20de59ceeb14c14686088a0e08
yasbir-ovik <yasbir.634551@gmail.com>
2025-12-11 04:38:19 +0600

    second commit

---
+++ JavaScript/greetBot.js
@@ -0,0 +1,21 @@
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+let bot;
+bot = "teacherBot";
+
+let botLocation = "the universe";
+
+console.log("Allow me to introduce myself.");
+
+const botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+
+const botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
+
+bot = "professorBot";
+
+const nicknameIntroduction = "My nickname is " + bot + ".";
+console.log(nicknameIntroduction);
+
+bot = "awesomeTeacherBot";
