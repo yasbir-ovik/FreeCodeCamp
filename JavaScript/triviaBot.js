4e09446daac0f650125e7a37a242df25d53f15ea
yasbir-ovik <yasbir.634551@gmail.com>
2025-12-11 05:49:28 +0600

    trivia bot

---
+++ JavaScript/triviaBot.js
@@ -0,0 +1,20 @@
+console.log("Hello! I'm your coding fun fact guide!")
+
+let botName  = "Alexa";
+let botLocation = "Earth";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is" +botName + "and I live on" + botLocation);
+console.log("My favorite programming language is" + favoriteLanguage);
+
+let codingFact = favoriteLanguage + "Is Not the Same as Java"
+console.log(codingFact);
+
+codingFact = favoriteLanguage + "Was Created in Just 10 Days"
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is interpreted, not compiled";
+console.log(codingFact);
+
+codingFact = "It was fun sharing these facts with you. Goodbye! -" + botName + " from " + botLocation + ".";
+console.log(codingFact);
