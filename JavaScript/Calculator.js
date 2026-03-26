5b6f6382284e8e684a74947fa5a5454481fdaef3
yasbir-ovik <yasbir.634551@gmail.com>
2026-03-26 06:08:09 +0600

    finished

--- JavaScript/Calculator.js
+++ JavaScript/Calculator.js
@@ -1,31 +1,31 @@
 function calculateSum(num1, num2) {
   return num1 + num2;
 }
-console.log(calculateSum(2, 5));
-console.log(calculateSum(10, 10));
-console.log(calculateSum(5, 5));
+
 // Sustract logic part
 function calculateDifference(num1, num2) {
   return num1 - num2;
 }
 
-console.log(calculateDifference(22, 5));
-console.log(calculateDifference(12, 1));
-console.log(calculateDifference(17, 9));
 // Multiply logic part
 function calculateProduct(num1, num2) {
   return num1 * num2;
 }
-console.log(calculateProduct(13, 5)); //
+//
 
 // Divide logic part
 function calculateQuotient(num1, num2) {
-  if(num2 === 0) {
+  if (num2 === 0) {
     return "Error: Division by zero";
   }
   return num1 / num2;
 }
-console.log(calculateQuotient(7, 11));
-console.log(calculateQuotient(3, 0));
 
-// Modulo logic part
+function calculateSquare(num) {
+  return Math.pow(num, 2);
+}
+
+//calculate square root
+function calculateSquareRoot(num) {
+  return Math.sqrt(num);
+}
